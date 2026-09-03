2c035073165dd76fcf3d27102b8a2f2da2bdb35c
Manish Malik <manishmalik6603@gmail.com>
2026-09-03 10:06:25 +0530

    Time: 12 ms (22.46%) | Memory: 121.7 MB (44.20%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -10,29 +10,36 @@ class Solution {
         if(counteven==n || counteven==0){
             return true;
         }
-        int[] nums2 = new int[n];
-        int smallestOdd= nums1[0];
-        for(int i=1;i<n;i++){
+        // int[] nums2 = new int[n];
+        int smallestOdd= Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
             if(nums1[i]%2 !=0 && nums1[i]<smallestOdd){
                     smallestOdd= nums1[i];
             }
 
         }
-        boolean flag=true;
-        for(int i=0;i<n;i++){
-            if(nums1[i] %2 !=0 ){
-                nums2[i]=nums1[i];
-            }else{
-                nums2[i]=nums1[i]-smallestOdd;
-                if(nums2[i]<=0){
-                    flag =false;
-                }
+        // boolean flag=true;
+        // for(int i=0;i<n;i++){
+        //     if(nums1[i] %2 !=0 ){
+        //         nums2[i]=nums1[i];
+        //     }else{
+        //         nums2[i]=nums1[i]-smallestOdd;
+        //         if(nums2[i]<=0){
+        //             flag =false;
+        //         }
 
                 
+        //     }
+        // }
+        for(int i=0;i<n;i++){
+            if(nums1[i] %2 ==0){
+                if(nums1[i] - smallestOdd <=0){
+                    return false;
+                }
             }
         }
 
-        return flag;
+        return true;
         
     }
 }
