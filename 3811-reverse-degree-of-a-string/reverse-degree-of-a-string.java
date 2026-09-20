805b133d6efc500a524b27396ae25c7ec18fc837
Manish Malik <manishmalik6603@gmail.com>
2026-09-20 20:41:52 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (34.29%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -3,21 +3,11 @@ class Solution {
         int totalSum = 0;
         
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            
-            // Check if the character is a letter
-           
-                // Convert to lowercase to handle both cases uniformly
-                char lowerCh = Character.toLowerCase(ch);
-                
-                // 'a' becomes 26, 'b' becomes 25, ..., 'z' becomes 1
-                int charValue = 26 - (lowerCh - 'a');
-                
-                // Position is 1-indexed, so we use (i + 1)
-                int position = i + 1;
-                
-                totalSum += charValue * position;
-            
+                    char ch = s.charAt(i);
+                    char lowerCh = Character.toLowerCase(ch);
+                    int charValue = 26 - (lowerCh - 'a');
+                    int position = i + 1;
+                    totalSum += charValue * position;
         }
         
         return totalSum;
